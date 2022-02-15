f1ad4a9762dba28eb8dd254d9e697d245635720e
YashRanjan2003 <60296034+YashRanjan2003@users.noreply.github.com>
2022-02-15 20:09:22 +0530

    Add files via upload

---
+++ botconfig.js
@@ -0,0 +1,44 @@
+module.exports = {
+  Admins: ["887598442787004436", "509632100950081544"], //Admins of the bot
+  ExpressServer: true, //If you wanted to make the website run or not
+  DefaultPrefix: process.env.Prefix || "-", //Default prefix, Server Admins can change the prefix
+  Port: 3000, //Which port website gonna be hosted
+  SupportServer: "https://discord.gg/WeFjMyeQDT", //Support Server Link
+  Token: process.env.Token || "OTQyNjUwNzIyMjcxNDk0MTU2.Ygnl5Q.7OVU7s1VoYWICUKds3aPBUGdXuM", //Discord Bot Token
+  ClientID: process.env.Discord_ClientID || "942650722271494156", //Discord Client ID
+  ClientSecret: process.env.Discord_ClientSecret || "FEbG5XOfCmCyI1pQ-yDvlEJhzPzJR0O5", //Discord Client Secret
+  Scopes: ["identify", "guilds", "applications.commands"], //Discord OAuth2 Scopes
+  ServerDeafen: true, //If you want bot to stay deafened
+  DefaultVolume: 100, //Sets the default volume of the bot, You can change this number anywhere from 1 to 100
+  CallbackURL: "/api/callback", //Discord API Callback url. Do not touch it if you don't know what you are doing. All you need to change for website to work is on line 20.
+  "24/7": true, //If you want the bot to be stay in the vc 24/7
+  CookieSecret: "Pikachu is cute", //A Secret like a password
+  IconURL:
+    "https://raw.githubusercontent.com/SudhanPlayz/Discord-MusicBot/master/assets/logo.gif", //URL of all embed author icons | Dont edit unless you dont need that Music CD Spining
+  EmbedColor: "GREEN", //Color of most embeds | Dont edit unless you want a specific color instead of a random one each time
+  Permissions: 2205281600, //Bot Inviting Permissions
+  Website: process.env.Website || "https://example.com", //Website where it is hosted at includes http or https || Use "0.0.0.0" if you using Heroku || Do not include /api/callback. Just the website url. I.e. "https://foo.bar"
+  //If you get invalid oauth, make sure on the discord developer page you set the oauth url to something like: https://example.com/api/callback.
+  
+  Presence: {
+    status: "online", // You can show online, idle, and dnd
+    name: "Music", // The message shown
+    type: "LISTENING", // PLAYING, WATCHING, LISTENING, STREAMING
+  },
+
+   // Lavalink server; public lavalink -> https://lavalink-list.darrennathanael.com/; create one yourself -> https://darrennathanael.com/post/how-to-lavalink/
+  // The default one should work fine, altho if you have issue with it, you can create your own or use another public lavalink.
+  Lavalink: {
+    id: "Main", //- Used for indentifier. You can set this to whatever you want.
+    host: "lavalink.islantay.tk", //- The host name or IP of the lavalink server.
+    port: 8880, // The port that lavalink is listening to. This must be a number!
+    pass: "waifufufufu", //- The password of the lavalink server.
+    secure: false, // Set this to true if the lavalink uses SSL. if not set it to false.
+  },
+
+  //Please go to https://developer.spotify.com/dashboard/
+  Spotify: {
+    ClientID: process.env.Spotify_ClientID || "a96a619e22c243e18fe8c4af9ff32eb5", //Spotify Client ID
+    ClientSecret: process.env.Spotify_ClientSecret || "0169073efb3143b6854d1d8c34be1595", //Spotify Client Secret
+  },
+};
